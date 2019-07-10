445df250e953504c8647640e7401d84c6dd64295
Adam Bolte <adam.bolte@sitepoint.com>
2019-07-10 15:56:02 +1000

    Remove lxc.kmsg and lxc.pivotdir references
    Support for these options have been removed since LXC 3.0.
    https://github.com/lxc/lxc/pull/1845

--- common/download.sh
+++ common/download.sh
@@ -63,7 +63,6 @@ then
   utils.lxc.stop
   echo  >> /var/lib/lxc/${CONTAINER}/config
   echo "# settings for systemd with PID 1:" >> /var/lib/lxc/${CONTAINER}/config
-  echo "lxc.kmsg = 0" >> /var/lib/lxc/${CONTAINER}/config
   echo "lxc.autodev = 1" >> /var/lib/lxc/${CONTAINER}/config
   utils.lxc.start
   utils.lxc.attach rm -f /dev/kmsg
